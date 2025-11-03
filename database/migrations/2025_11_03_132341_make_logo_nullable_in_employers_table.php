f06a1528c2f6b7a55982d9842c9752f50e7bfab1
Gaurav2346 <gauravbhoir62@gmail.com>
2025-11-03 18:59:14 +0530

    New Changes 2.0

--- database/migrations/2025_11_03_132341_make_logo_nullable_in_employers_table.php
+++ database/migrations/2025_11_03_132341_make_logo_nullable_in_employers_table.php
@@ -6,23 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::table('employers', function (Blueprint $table) {
-            //
+            $table->string('logo')->nullable()->change();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::table('employers', function (Blueprint $table) {
-            //
+            $table->string('logo')->nullable(false)->change();
         });
     }
 };
