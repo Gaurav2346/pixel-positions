b7c403ea927c9928b8555a99641216e0e76c0e6c
Gaurav2346 <gauravbhoir62@gmail.com>
2025-11-03 19:31:15 +0530

    trying to change logo setting

--- resources/views/components/employer-logo.blade.php
+++ resources/views/components/employer-logo.blade.php
@@ -1,14 +1,21 @@
 @props(['employer', 'width' => 100])
 
 @php
-    $logo = filter_var($employer->logo, FILTER_VALIDATE_URL)
-        ? $employer->logo
-        : asset('storage/' . $employer->logo);
+    $logo = $employer->logo;
+
+    // Detect if it's not a full URL (i.e. a stored file name)
+    if (! filter_var($logo, FILTER_VALIDATE_URL)) {
+        $logo = asset('storage/' . ltrim($logo, '/'));
+    }
+//    // Optional fallback image if null
+//    if (empty($logo)) {
+//        $logo = 'https://via.placeholder.com/150?text=No+Logo';
+//    }
 @endphp
 
 <img
     src="{{ $logo }}"
     width="{{ $width }}"
-    alt="{{ $employer->name }}"
+    alt="{{ $employer->name ?? 'Employer Logo' }}"
     class="rounded-xl object-cover"
 />
